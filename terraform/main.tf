5c6680027236f1470f8537a49cd5e9130e85d534
unbeatable-abayomi <igwubor@outlook.com>
2026-04-08 12:47:26 +0100

    Updated Main.tf and Output.tf

---
+++ terraform/main.tf
@@ -0,0 +1,95 @@
+provider "azurerm" {
+features {}
+}
+resource "azurerm_resource_group" "rg" {
+name = var.resource_group_name
+location = var.location
+}
+resource "azurerm_virtual_network" "vnet" {
+name = "finance-vnet"
+address_space = ["10.0.0.0/16"]
+location = var.location
+resource_group_name = azurerm_resource_group.rg.name
+}
+resource "azurerm_subnet" "subnet" {
+name = "finance-subnet"
+resource_group_name = azurerm_resource_group.rg.name
+virtual_network_name = azurerm_virtual_network.vnet.name
+address_prefixes = ["10.0.1.0/24"]
+}
+
+
+resource "azurerm_network_security_group" "nsg" {
+name = "finance-nsg"
+location = var.location
+resource_group_name = azurerm_resource_group.rg.name
+security_rule {
+name = "SSH"
+priority = 1001
+direction = "Inbound"
+access = "Allow"
+protocol = "Tcp"
+source_port_range = "*"
+destination_port_range = "22"
+source_address_prefix = "*"
+destination_address_prefix = "*"
+}
+security_rule {
+name = "HTTP"
+priority = 1002
+direction = "Inbound"
+access = "Allow"
+protocol = "Tcp"
+source_port_range = "*"
+destination_port_range = "80"
+source_address_prefix = "*"
+destination_address_prefix = "*"
+}
+}
+resource "azurerm_public_ip" "pip" {
+name = "finance-pip"
+location = var.location
+resource_group_name = azurerm_resource_group.rg.name
+allocation_method = "Static"
+sku = "Standard"
+}
+resource "azurerm_network_interface" "nic" {
+name = "finance-nic"
+location = var.location
+resource_group_name = azurerm_resource_group.rg.name
+ip_configuration {
+name = "internal"
+subnet_id = azurerm_subnet.subnet.id
+private_ip_address_allocation = "Dynamic"
+public_ip_address_id = azurerm_public_ip.pip.id
+}
+}
+
+resource "azurerm_network_interface_security_group_association" "nic_nsg"{
+network_interface_id = azurerm_network_interface.nic.id
+network_security_group_id = azurerm_network_security_group.nsg.id
+}
+resource "azurerm_linux_virtual_machine" "vm" {
+name = var.vm_name
+resource_group_name = azurerm_resource_group.rg.name
+location = var.location
+size = "Standard_B1s"
+admin_username = var.admin_username
+network_interface_ids = [azurerm_network_interface.nic.id]
+os_disk {
+caching = "ReadWrite"
+storage_account_type = "Standard_LRS"
+name = "${var.vm_name}-disk"
+}
+source_image_reference {
+publisher = "Canonical"
+offer = "0001-com-ubuntu-server-jammy"
+sku = "22_04-lts"
+version = "latest"
+}
+admin_ssh_key {
+username = var.admin_username
+public_key = file(var.ssh_public_key)
+}
+disable_password_authentication = true
+}
